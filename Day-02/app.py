19e0992475b43e7a15a43d642addb0b7ac862e5c
Madhu Parvathaneni <madhu@codegnan.com>
2026-08-04 13:04:43 +0530

    updating day2 class

---
+++ Day-02/app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+# routes create
+@app.route('/')
+def home():
+    return 'PFS-HYD-054 was brilliant'
+
+@app.route('/batchno')
+def batchno():
+    return str(54)
+
+@app.route('/index')
+def indexPage():
+    return render_template('index.html')
+
+@app.route('/about')
+def aboutPage():
+    return render_template('about.html')
+
+if (__name__=="__main__"):
+    app.run(
+        host='0.0.0.0',
+        port=5001,
+        debug=True
+    )
+
