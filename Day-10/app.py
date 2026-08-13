942d13aa87990d662d81cb9f0e7d2cb1947590e4
Madhu Parvathaneni <madhu@codegnan.com>
2026-08-13 11:38:32 +0530

    adding day10 class

---
+++ Day-10/app.py
@@ -0,0 +1,133 @@
+from config import Config
+import mysql.connector as sql
+
+DBConfig=Config()
+
+def getConnectionWithDB():
+    db_host=DBConfig.db_host
+    db_port=DBConfig.db_port
+    db_user=DBConfig.db_user
+    db_password=DBConfig.db_password
+    db_name=DBConfig.db_name
+    try:
+        connection=sql.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        return connection
+    except:
+        return 'Connection Failed'
+
+def insertUserRecord(user_data):
+    name=user_data['name']
+    email=user_data['email']
+    password_hash=user_data['password_hash']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        try:
+            cursor=connection.cursor()
+            cursor.execute("INSERT INTO users (name,email,password_hash) values(%s,%s,%s)",(name,email,password_hash))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return True
+        except:
+            print('Data cant be inserted')
+            return False
+
+def readUserRecords():
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        cursor.execute("SELECT * from users")
+        data=cursor.fetchall()
+        records=[]
+        for record in data:
+            temp={}
+            temp['id']=record[0]
+            temp['name']=record[1]
+            temp['email']=record[2]
+            temp['password_hash']=record[3]
+            temp['is_verified']=record[4]
+            temp['created_at']=record[5]
+            records.append(temp)
+        cursor.close()
+        connection.close()
+        return records
+
+def readUserRecordByEmail(user_data):
+    email=user_data['email']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        cursor.execute("SELECT * from users where email=%s",(email,))
+        data=cursor.fetchone()
+        try:
+            record={
+                'id':data[0],
+                'name':data[1],
+                'email':data[2],
+                'password_hash':data[3],
+                'is_verified':data[4],
+                'created_at':data[5]
+            }
+            cursor.close()
+            connection.close()
+            return record
+        except:
+            cursor.close()
+            connection.close()
+            return 'No record'
+
+def readUserRecordById(user_data):
+    id=user_data['id']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        cursor.execute("SELECT * from users where id=%s",(id,))
+        data=cursor.fetchone()
+        try:
+            record={
+                'id':data[0],
+                'name':data[1],
+                'email':data[2],
+                'password_hash':data[3],
+                'is_verified':data[4],
+                'created_at':data[5]
+            }
+            cursor.close()
+            connection.close()
+            return record
+        except:
+            cursor.close()
+            connection.close()
+            return 'No record'
+
+def updateNameByIdorEmail():
+    pass
+
+def updatePasswordByIdorEmail():
+    pass
+
+def updateIsVerifiedByIdorEmail():
+    pass
+
+def generateOTP():
+    pass
+
+def sendOTPviaEmail():
+    pass
+
+user_data={'id':2}
+print(readUserRecordById(user_data))
