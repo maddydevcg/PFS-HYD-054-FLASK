91d9e0470568748497e3e6a9df0fab471e2f65c0
Madhu Parvathaneni <madhu@codegnan.com>
2026-08-14 15:50:24 +0530

    updating Day11 class

---
+++ Day-11/app.py
@@ -0,0 +1,209 @@
+from config import Config
+import mysql.connector as sql
+import random
+
+DBConfig=Config()
+
+def getConnectionWithDB():
+    db_host=DBConfig.db_host
+    db_port=DBConfig.db_port
+    db_user=DBConfig.db_user
+    db_password=DBConfig.db_password
+    db_name=DBConfig.db_name
+    try:
+        connection=sql.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        return connection
+    except:
+        return 'Connection Failed'
+
+def insertUserRecord(user_data):
+    name=user_data['name']
+    email=user_data['email']
+    password_hash=user_data['password_hash']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        try:
+            cursor=connection.cursor()
+            cursor.execute("INSERT INTO users (name,email,password_hash) values(%s,%s,%s)",(name,email,password_hash))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return True
+        except:
+            print('Data cant be inserted')
+            return False
+
+def readUserRecords():
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        cursor.execute("SELECT * from users")
+        data=cursor.fetchall()
+        records=[]
+        for record in data:
+            temp={}
+            temp['id']=record[0]
+            temp['name']=record[1]
+            temp['email']=record[2]
+            temp['password_hash']=record[3]
+            temp['is_verified']=record[4]
+            temp['created_at']=record[5]
+            records.append(temp)
+        cursor.close()
+        connection.close()
+        return records
+
+def readUserRecordByEmail(user_data):
+    email=user_data['email']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        cursor.execute("SELECT * from users where email=%s",(email,))
+        data=cursor.fetchone()
+        try:
+            record={
+                'id':data[0],
+                'name':data[1],
+                'email':data[2],
+                'password_hash':data[3],
+                'is_verified':data[4],
+                'created_at':data[5]
+            }
+            cursor.close()
+            connection.close()
+            return record
+        except:
+            cursor.close()
+            connection.close()
+            return 'No record'
+
+def readUserRecordById(user_data):
+    id=user_data['id']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        cursor.execute("SELECT * from users where id=%s",(id,))
+        data=cursor.fetchone()
+        try:
+            record={
+                'id':data[0],
+                'name':data[1],
+                'email':data[2],
+                'password_hash':data[3],
+                'is_verified':data[4],
+                'created_at':data[5]
+            }
+            cursor.close()
+            connection.close()
+            return record
+        except:
+            cursor.close()
+            connection.close()
+            return 'No record'
+
+def updateNameByIdorEmail(user_data):
+    query_filter=''
+    try:
+        id=user_data['id']
+        query_filter='id'
+    except:
+        email=user_data['email']
+        query_filter='email'
+    new_name=user_data['new_name']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        if query_filter=='id':
+            query="UPDATE users SET name=%s WHERE id=%s"
+            values=(new_name,id)
+        elif query_filter=='email':
+            query="UPDATE users SET name=%s WHERE email=%s"
+            values=(new_name,email)
+        cursor.execute(query,values)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return True
+
+
+def updatePasswordByIdorEmail(user_data):
+    query_filter=''
+    try:
+        id=user_data['id']
+        query_filter='id'
+    except:
+        email=user_data['email']
+        query_filter='email'
+    new_password=user_data['new_password']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        if query_filter=='id':
+            query="UPDATE users SET password_hash=%s where id=%s"
+            values=(new_password,id)
+        elif query_filter=='email':
+            query="UPDATE users SET password_hash=%s where email=%s"
+            values=(new_password,email)
+        cursor.execute(query,values)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return True
+
+def updateIsVerifiedByIdorEmail(user_data):
+    query_filter=''
+    try:
+        id=user_data['id']
+        query_filter='id'
+    except:
+        email=user_data['email']
+        query_filter='email'
+    is_verified=user_data['is_verified']
+    connection=getConnectionWithDB()
+    if connection=='Connection Failed':
+        return False
+    else:
+        cursor=connection.cursor()
+        if query_filter=='id':
+            query="UPDATE users SET is_verified=%s WHERE id=%s"
+            values=(is_verified,id)
+        elif query_filter=='email':
+            query="UPDATE users SET is_verified=%s WHERE email=%s"
+            values=(is_verified,email)
+        cursor.execute(query,values)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return True
+
+def generateOTP():
+    otp=random.randint(1000,9999)
+    return otp
+
+def sendOTPviaEmail():
+    # Step-1: To whom, we have to send OTP
+    # Step-2: Through which account we have to send OTP
+    # Step-3: FROM - GMAIL LOGIN - APP PASSWORD
+    # Step-4: Mail Compose
+    # Step-5: Mail Send
+    pass
+
+print(generateOTP())
